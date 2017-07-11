03797c7d44e2b356935d11469d5995e7bccc6184
shh10 <shh10@sfu.ca>
2017-07-11 12:41:56 -0700

    Add files via upload

---
+++ tutoria-correct-js-starter/js/menu.js
@@ -0,0 +1,15 @@
+//this is my js file//
+
+/*I am a comment with more than one line.*/
+
+function myFunction() {
+	var x = document.getElementByID('menu');
+	if (x.style.display === 'none') {
+		x.style.display = 'block';
+	}
+
+	else {
+		x.style.display = 'none';
+	}
+
+	}
